2c27a3a6b84bc0ad5e5eef776305a87d25b70df3
MayankDKarmakar <karmakar.d.mayank@gmail.com>
2023-04-02 00:40:39 +0530

    fixed favicon and Header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import Exchanges from './components/Exchanges';
-import Coins from './components/Coins';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Exchanges from "./components/Exchanges";
+import Coins from "./components/Coins";
 
 const App = () => {
   return (
     <Router>
-    <Header />
-    <Routes>
-    <Route path='/' element={<Home />} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coins" element={<Coins />} />
+      </Routes>
     </Router>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Box, Button, HStack, Image } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <HStack
+      p={"4"}
+      bgColor={"#130f40"}
+      justifyContent={"space-between"}
+      shadow={"base"}
+    >
+      <Image src={img} boxSize={"10"} />
 
-export default Header
+      <Box px={'8'} >
+        <HStack>
+          <Button variant={"link"} color={"white"}>
+            <Link to={"/"}>Home</Link>
+          </Button>
+          <Button variant={"link"} color={"whiteAlpha.900"}>
+            <Link to={"/exchanges"}>Exchanges</Link>
+          </Button>
+          <Button variant={"link"} color={"whiteAlpha.900"}>
+            <Link to={"/coins"}>Coins</Link>
+          </Button>
+        </HStack>
+      </Box>
+    </HStack>
+  );
+};
+
+export default Header;
