3cd3f184f4967c16aa43c34cdbd3beb6b03e3b88
MayankDKarmakar <karmakar.d.mayank@gmail.com>
2023-04-05 18:40:12 +0530

    fixed Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 import React from "react";
 import { motion } from "framer-motion";
 import img from "../assets/image.png";
